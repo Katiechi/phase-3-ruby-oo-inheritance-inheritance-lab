4a73882527a9f4b5d9f89f77a53bdee9275adb87
brian ambundo <brian.katiechi9@gmail.com>
2022-08-24 08:46:57 +0300

    commit tree 0919ede18e8b449e8d5edc90aad5180a3335a4c9

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative './user'
 
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn (argument)
+        @knowledge << argument
+    end
 end
